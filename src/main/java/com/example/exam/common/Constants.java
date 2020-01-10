7deddf423fd7d083d3211c658d370807d79d30e8
StefanUrilski <urilskistefan@gmail.com>
2020-01-10 12:35:33 +0200

    Exception classes for register user created.

---
+++ src/main/java/com/example/exam/common/Constants.java
@@ -0,0 +1,12 @@
+package com.example.exam.common;
+
+public final class Constants {
+
+    public static final String WRONG_NON_EXISTENT_ID = "Wrong or non-existent id.";
+
+    public static final String WRONG_NON_EXISTENT_USER = "Wrong or non-existent user.";
+
+    public static final String NON_EXISTENT_ROLE = "Non-existent role.";
+
+    public static final String USER_REGISTER_EXCEPTION = "User with that name already exists!";
+}
---
+++ src/main/java/com/example/exam/errors/IdNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.exam.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Non-existent id.")
+public class IdNotFoundException extends RuntimeException {
+
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
