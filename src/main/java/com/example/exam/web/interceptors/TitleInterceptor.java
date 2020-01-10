3e19381341cf710e422313562174f72fcdefac41
StefanUrilski <urilskistefan@gmail.com>
2020-01-10 12:39:27 +0200

    Title interceptor class created.

---
+++ src/main/java/com/example/exam/web/interceptors/TitleInterceptor.java
@@ -0,0 +1,23 @@
+package com.example.exam.web.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TitleInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        String title = "ISTQBR Certificate";
+
+        if (modelAndView == null) {
+            modelAndView = new ModelAndView();
+        }
+
+        modelAndView.addObject("title", title);
+    }
+}
