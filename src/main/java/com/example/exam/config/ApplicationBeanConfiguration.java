6dc94e23b8bc2986dd9d9b8fba91b859e7b0de63
StefanUrilski <urilskistefan@gmail.com>
2020-01-13 09:21:38 +0200

    Added bean

--- src/main/java/com/example/exam/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/example/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.exam.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,5 +18,11 @@ public class ApplicationBeanConfiguration {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor removeTomcatWebServerCustomizer() {
+        return (beanFactory) ->
+                ((DefaultListableBeanFactory)beanFactory).removeBeanDefinition("tomcatWebServerFactoryCustomizer");
     }
 }
