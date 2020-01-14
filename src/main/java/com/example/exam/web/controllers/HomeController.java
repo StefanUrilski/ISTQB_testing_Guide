4848741de47c05d5b28f6fced78ce418bac9f890
StefanUrilski <urilskistefan@gmail.com>
2020-01-14 20:13:27 +0200

    For testing purposes only.

--- src/main/java/com/example/exam/web/controllers/HomeController.java
+++ src/main/java/com/example/exam/web/controllers/HomeController.java
@@ -3,7 +3,10 @@ package com.example.exam.web.controllers;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
 
 @Controller
 public class HomeController extends BaseController {
@@ -18,6 +21,14 @@ public class HomeController extends BaseController {
     @PreAuthorize("isAuthenticated()")
     public ModelAndView getQuestions() {
         return view("home");
+    }
+
+    @GetMapping("/1")
+    @ResponseBody
+    public String path() {
+        File file = new File("");
+
+        return file.getAbsolutePath();
     }
 
 }
