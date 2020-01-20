4bc32c2c992f704272ccd590b7e5d54a7b6911bf
StefanUrilski <urilskistefan@gmail.com>
2020-01-20 17:44:49 +0200

    Figure entity created.

---
+++ src/main/java/com/example/exam/domain/entities/Figure.java
@@ -0,0 +1,31 @@
+package com.example.exam.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "figures")
+public class Figure extends BaseEntity {
+
+    private String url;
+    private String name;
+
+    @Column(columnDefinition = "url", nullable = false)
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Column(columnDefinition = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
