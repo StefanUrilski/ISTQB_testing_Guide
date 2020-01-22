0f4860dc2f49ac588fd8f0ad55e7822f61ce1cc9
StefanUrilski <urilskistefan@gmail.com>
2020-01-22 14:52:51 +0200

    AllQuestionVisitedException exception class created.

--- src/main/java/com/example/exam/errors/AllQuestionVisitedException.java
+++ src/main/java/com/example/exam/errors/AllQuestionVisitedException.java
@@ -3,7 +3,7 @@ package com.example.exam.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Already existed file.")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "All questions are visited.")
 public class AllQuestionVisitedException extends RuntimeException {
 
     public AllQuestionVisitedException() {
