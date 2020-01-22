ee0e46b4aa23c6a49c5531a65a2f22c92c135ee4
StefanUrilski <urilskistefan@gmail.com>
2020-01-22 15:33:51 +0200

    Bug fixed on getTenUniqueRandomNumbers() method.

--- src/main/java/com/example/exam/util/RandomProvider.java
+++ src/main/java/com/example/exam/util/RandomProvider.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public interface RandomProvider {
 
     /**
-     * Returns list of ten random integers in range from between zero (inclusive)
+     * Returns set of ten unique random integers in range from between zero (inclusive)
      * and {@code maxNumber} (exclusive). Uses random number generator, from
      * Random class. Method made only for testing purposes.
      *
